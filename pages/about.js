3cd6f009207ccf15a878940852e433b8448d8f9d
Morgan Njaw <morgannjaw1998@gmail.com>
2021-09-13 20:04:16 +0100

    first commit

---
+++ pages/about.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+const About = () => {
+    return ( 
+        <>
+            <Head>
+            <title>Ninja List | About </title>
+            <meta name="keywords" content="ninjas" />
+        </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet consecteur adipiscing</p>
+                <p>Lorem ipsum dolor sit amet consecteur adipiscing</p>
+                
+            </div>
+        </>
+     );
+}
+ 
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Footer from '../comps/Footer'
+import Navbar from '../comps/Navbar'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | Home </title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div >
+        <h1 className={styles.title}>HomePage</h1>
+        <p className={styles.text}>Lorem ipsum dolor sit amet consecteur adipiscing</p>
+        <p className={styles.text}>Lorem ipsum dolor sit amet consecteur adipiscing</p>
+        
+        <Link href="/ninjas">
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+      </>
+    )
+  }
